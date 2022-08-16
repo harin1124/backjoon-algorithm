ab633cf11604892382013454e963990c5864cbed
harin1124 <lolrin1124@gmail.com>
2022-08-17 01:02:58 +0900

    1316번 문제 <그룹 단어 체커> 문제 해결

--- src/backjoon/problem1316.java
+++ src/backjoon/problem1316.java
@@ -12,31 +12,35 @@ import java.util.Scanner;
           단어는 알파벳 소문자로만 되어있고 중복되지 않으며, 길이는 최대 100이다.
  * @output 첫째 줄에 그룹 단어의 개수를 출력한다.
  * @studyStartDate 2022-08-16
- * @studyEndDate 2022-08-
+ * @studyEndDate 2022-08-17
  */
 public class problem1316 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int size = scan.nextInt();
+		int size = Integer.parseInt(scan.nextLine());
 
-		/*
-		4
-		aba
-		abab
-		abcabc
-		a
-		*/
 		String[] list = new String[size];
+
+		String str = "";
+		String nextStr = "";
+		int result = size;
+
 		for(int i=0; i<size; i++){
 			list[i] = scan.nextLine();
-		}
-		
-		// 추후 for문 안에 들어가도 됌 지금은 테스트라 여기 위치시킴
-		for(int i=0; i<size; i++){
-			System.out.println(list[i]);
-			for(int j=0; j<list[i].length(); j++){
-				System.out.println("\t"+list[i].charAt(j));
+			str = String.valueOf(list[i].charAt(0));
+			for(int j=1; j<list[i].length(); j++){
+				nextStr = String.valueOf(list[i].charAt(j));
+				// str에 nextStr(다음문자열)이 포함되어 있지 않을 때 || str에 nextStr이 포함되어 있고, str의 마지막 문자열과 nextStr이 동일할 때 => 같은 문자가 반복되므로 그룹인 것이니까
+				if(!str.contains(nextStr) || ( str.contains(nextStr) && String.valueOf(str.charAt(str.length()-1)).equals(nextStr) )){
+					str += nextStr;
+				}else{
+					result--;
+					break;
+				}
 			}
+			str = "";
+			nextStr = "";
 		}
+		System.out.println(result);
 	}
 }
