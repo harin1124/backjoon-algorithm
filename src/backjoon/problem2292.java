d65f50130b92ca8419e64525d1c436d249d967e3
harin1124 <lolrin1124@gmail.com>
2022-08-22 17:24:52 +0900

    문제 임시 커밋

--- src/backjoon/problem2292.java
+++ src/backjoon/problem2292.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
  * @input 첫째 줄에 N(1 ≤ N ≤ 1,000,000,000)이 주어진다.
  * @output 입력으로 주어진 방까지 최소 개수의 방을 지나서 갈 때 몇 개의 방을 지나는지 출력한다.
  * @studyStartDate 2022-08-18
- * @studyEndDate 2022-08-
+ * @studyEndDate 2022-08-18
  */
 public class problem2292 {
 	public static void main(String[] args) {
