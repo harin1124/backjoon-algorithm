f012a398e78ecd499a1d4ddeab685a2deb030366
harin1124 <lolrin1124@gmail.com>
2022-08-23 10:49:00 +0900

    분수 문제 임시 커밋

--- src/backjoon/problem1193.java
+++ src/backjoon/problem1193.java
@@ -24,32 +24,79 @@ public class problem1193 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		//int num = Integer.valueOf(br.readLine());
-		int num = 6;
+		int num = 9;
 		br.close();
 
-		int row = 1;
-		int col = 1;
+		int row = 0;
+		int col = 0;
 
-		// ->  col++
-		// /   row++ col--
-		// \   row-- col++
-		// |   row++ col
-
-		// 패턴
-		// -> 1 / 1
-
-		System.out.println(num);
+		int a = 0;  // 수평 -> 이동
+		int b = 0;  // 좌측 아래 대각선 이동
+		int c = 0;  // 수직 이동
+		int d = 0;  // 우측 위 대각선 이동
 
 		int 몫 = num / 4;
 		int 나머지 = num % 4;
 
+		몫 = 2;
+		나머지 = 2;
+
 		System.out.println("몫 : "+몫);
 		System.out.println("나머지 : "+나머지);
 
-		// -> 방향
-		col = col + 6;
-		// /
-		//row =
+		if(몫 == 0){
+			// 작게 나오면 어떻게해야 하나...?
+			if(나머지 <= 1){
+				a++;
+			}
+			if(나머지 <= 2){
+				b = b+2;
+			}
+			if(나머지 <= 3){
+				c++;
+			}
+		}else{
+			// 몫 구하기
+			a = 몫;
+			c = 몫;
+
+			b = 몫 * 몫; // 몫은 4라는 것으로 16까지 숫자 얻어내기
+			d = b + 몫;
+
+			System.out.println("=====================");
+			System.out.println("a : "+a);
+			System.out.println("b : "+b);
+			System.out.println("c : "+c);
+			System.out.println("d : "+d);
+
+			if(나머지 > 0){
+				if(나머지 >= 1){
+					a++;
+				}
+				if(나머지 >= 2){
+					b = b+2;
+				}
+				if(나머지 >= 3){
+					c++;
+				}
+			}
+			System.out.println("=====================");
+			System.out.println("a : "+a);
+			System.out.println("b : "+b);
+			System.out.println("c : "+c);
+			System.out.println("d : "+d);
+
+			col = 1 + a + b - d;
+			row = 1 + c + d - b;
+
+
+
+			System.out.println("최종 결과 =====================");
+			System.out.println("a : "+a);
+			System.out.println("b : "+b);
+			System.out.println("c : "+c);
+			System.out.println("d : "+d);
+		}
 
 		System.out.println(row+"/"+col);
 	}
