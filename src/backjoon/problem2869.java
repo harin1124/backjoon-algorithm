889e90a8dfdd37147a85e8c4757eaa5e6dac5ddf
harin1124 <lolrin1124@gmail.com>
2022-09-05 21:39:09 +0900

    달팽이 문제 임시 커밋

--- src/backjoon/problem2869.java
+++ src/backjoon/problem2869.java
@@ -1,15 +1,29 @@
 package backjoon;
 
+import java.util.Scanner;
+
 /**
- * @title 벌집
+ * @title 달팽이는 올라가고 싶다
  * @desc 땅 위에 달팽이가 있다. 이 달팽이는 높이가 V미터인 나무 막대를 올라갈 것이다.
-         달팽이는 낮에 A미터 올라갈 수 있다. 하지만, 밤에 잠을 자는 동안 B미터 미끄러진다.
-         또, 정상에 올라간 후에는 미끄러지지 않는다.
-         달팽이가 나무 막대를 모두 올라가려면, 며칠이 걸리는지 구하는 프로그램을 작성하시오.
+달팽이는 낮에 A미터 올라갈 수 있다. 하지만, 밤에 잠을 자는 동안 B미터 미끄러진다.
+또, 정상에 올라간 후에는 미끄러지지 않는다.
+달팽이가 나무 막대를 모두 올라가려면, 며칠이 걸리는지 구하는 프로그램을 작성하시오.
  * @input 첫째 줄에 세 정수 A, B, V가 공백으로 구분되어서 주어진다. (1 ≤ B < A ≤ V ≤ 1,000,000,000)
  * @output 첫째 줄에 달팽이가 나무 막대를 모두 올라가는데 며칠이 걸리는지 출력한다.
  * @studyStartDate 2022-08-25
  * @studyEndDate 2022-08-
  */
 public class problem2869 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int up = scan.nextInt();
+		int down = scan.nextInt();
+		int height = scan.nextInt();
+
+		//System.out.println("up : "+up);
+		//System.out.println("down : "+down);
+		//System.out.println("height : "+height);
+
+		System.out.println(height - (up-down));
+	}
 }
