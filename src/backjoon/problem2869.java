a7bde0c2039676d94d37364d0aa1500cf0da9dd9
harin1124 <lolrin1124@gmail.com>
2022-10-04 19:56:11 +0900

    2869번 문제 <달팽이는 올라가고 싶다> 문제 해결 및 해설 추가

--- src/backjoon/problem2869.java
+++ src/backjoon/problem2869.java
@@ -1,6 +1,7 @@
 package backjoon;
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 /**
  * @title 달팽이는 올라가고 싶다
@@ -11,19 +12,26 @@ import java.util.Scanner;
  * @input 첫째 줄에 세 정수 A, B, V가 공백으로 구분되어서 주어진다. (1 ≤ B < A ≤ V ≤ 1,000,000,000)
  * @output 첫째 줄에 달팽이가 나무 막대를 모두 올라가는데 며칠이 걸리는지 출력한다.
  * @studyStartDate 2022-08-25
- * @studyEndDate 2022-08-
+ * @studyEndDate 2022-10-04
  */
 public class problem2869 {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int up = scan.nextInt();
-		int down = scan.nextInt();
-		int height = scan.nextInt();
+	public static void main(String[] args) throws IOException {
+		// 메모리 절약을 위해 BufferedReader와 StringTokenizer 사용
+		BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int v = Integer.parseInt(st.nextToken());
 
-		//System.out.println("up : "+up);
-		//System.out.println("down : "+down);
-		//System.out.println("height : "+height);
-
-		System.out.println(height - (up-down));
+		// 무조건 오전의 반나절 올라가는 것으로 끝나기 때문에
+		// 올라가는 것은 전체 높이에서 올라가는 것을 뺀 높이 기준으로 계산하고 + 1 을 해준다.
+		// v-a = 전체 높이에서 올라가는 것을 뺀 높이로, 마지막에 올라갈 A를 남겨두는 것
+		// a-b = 하루에 올라갈 수 있는 높이
+		// => v-a / a-b 는 v-a를 제외한 높이를 올라가는 데 걸리는 일 수 이며,
+		// => 해당 일 수가 예를 들어 7.2일이면 7.2일은 내림이 불가능하고 무조건 하루로 쳐서
+		// => (double) 형변환 후, Math.ceil로 숫자를 올림 처리를 하고 다시 int 자료형으로 바꾼다.
+		// 그리고 마지막에 올라갈 A를 남겨두었으므로 해당 결과에 +1을 한다.
+		int res = (int)Math.ceil((double)(v-a)/(a-b));
+		System.out.println(res+1);
 	}
 }
