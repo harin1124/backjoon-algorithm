7aae4fa345952606dd5c8ff7ab50be6b550f1e6d
harin1124 <lolrin1124@gmail.com>
2023-01-08 21:46:54 +0900

    인프런 자바 알고리즘 <3. 문장 속 단어> 강의 듣고 난 후 재풀이

--- src/inflearn_java/Problem_01_03_02.java
+++ src/inflearn_java/Problem_01_03_02.java
@@ -11,26 +11,36 @@ import java.io.OutputStreamWriter;
  *       문장속의 각 단어는 공백으로 구분됩니다.
  * @input 첫 줄에 길이가 100을 넘지 않는 한 개의 문장이 주어집니다. 문장은 영어 알파벳으로만 구성되어 있습니다.
  * @output 첫 줄에 가장 긴 단어를 출력한다. 가장 길이가 긴 단어가 여러개일 경우 문장속에서 가장 앞쪽에 위치한 단어를 답으로 합니다.
- * @studyStartDate 2023-01-07
- * @studyEndDate 2023-01-07
+ * @studyStartDate 2023-01-08
+ * @studyEndDate 2023-01-08
  */
-public class Problem_01_03 {
+public class Problem_01_03_02 {
 	public static void main(String[] args) throws Exception {
+		// 강의에서 나온, 포인터 개념으로 푸는 방법
 		BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		// 문자열을 받아서 공백 기준으로 문자열 배열 생성
-		String[] words = br.readLine().split(" ");
-		int len = 0;
-		int idx = 0;
-		// 뒤에서부터 반복문으로 값 찾기 => 가장 길이가 긴 단어가 여러개일 때 문장속에서 가장 앞쪽에 위치한 단어가 답이므로, 뒤에서부터 찾음
-		for(int i=words.length-1; i>=0; i--){
-			if(words[i].length() >= len){
-				len = words[i].length();
-				idx = i;
+		String str = br.readLine();
+		String tmp;
+		String result = "";
+		int minLength = 0;
+		int pos = 0;
+
+		while((pos = str.indexOf(" ")) != -1){
+			tmp = str.substring(0, pos); // 0번째부터 pos의 값의 전까지 문자열 잘라내기
+			if(tmp.length() > minLength){
+				minLength = tmp.length();
+				result = tmp;
 			}
+			// 포인터 기준으로 다음 번에 확인할 문자열 재정의
+			str = str.substring(pos+1);
 		}
-		bw.write(words[idx]+"");
+		// 마지막 나머지 검사
+		if(str.length() > minLength){
+			result = str;
+		}
+
+		bw.write(result+"");
 		br.close();
 		bw.close();
 	}
