0356a05eed2de047ba1b49a88131417958ba7942
이하린 <ihalin@ihalin-ui-MacBookAir.local>
2023-01-10 12:39:52 +0900

    인프런 자바 알고리즘 <6. 중복문자제거> 풀이 완료

--- src/inflearn_java/Problem_01_06.java
+++ src/inflearn_java/Problem_01_06.java
@@ -12,7 +12,7 @@ import java.io.OutputStreamWriter;
  * @input 첫 줄에 문자열이 입력됩니다. 문자열의 길이는 100을 넘지 않는다.
  * @output 첫 줄에 중복문자가 제거된 문자열을 출력합니다.
  * @studyStartDate 2023-01-09
- * @studyEndDate 2023-01-
+ * @studyEndDate 2023-01-10
  */
 public class Problem_01_06 {
 	public static void main(String[] args) throws Exception {
@@ -20,23 +20,9 @@ public class Problem_01_06 {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		String str = br.readLine();
-		String repeat = String.valueOf(str.charAt(0));
-		String repeatChkStr = "";
-		String temp = "";
-		int size = 0;
 		for(int i=0; i<str.length(); i++){
-			temp = String.valueOf(str.charAt(i));
-			System.out.println(temp);
-			if(repeat.indexOf(temp) == 0){
-				size = repeat.length();
-				repeatChkStr = temp;
-			} else {
-				repeat += temp;
-				repeatChkStr += temp;
-			}
-			if(size > 0 && size == repeatChkStr.length()){
-				bw.write(repeatChkStr);
-				break;
+			if(i == str.indexOf(str.charAt(i))){
+				bw.write(str.charAt(i));
 			}
 		}
 		bw.close();
