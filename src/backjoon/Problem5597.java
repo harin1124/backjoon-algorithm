67a87faee717b8398b6213c2874660126e443058
harin1124 <lolrin1124@gmail.com>
2025-07-20 00:09:43 +0900

    5597번 문제 <과제 안 내신 분..?> 풀이 완료

---
+++ src/backjoon/Problem5597.java
@@ -0,0 +1,33 @@
+package backjoon;
+
+import java.util.Scanner;
+
+/**
+ * @title 과제 안 내신 분..?
+ * @desc X대학 M교수님은 프로그래밍 수업을 맡고 있다.<br>
+ * 교실엔 학생이 30명이 있는데, 학생 명부엔 각 학생별로 1번부터 30번까지 출석번호가 붙어 있다.<br>
+ * 교수님이 내준 특별과제를 28명이 제출했는데, 그 중에서 제출 안 한 학생 2명의 출석번호를 구하는 프로그램을 작성하시오.
+ * @input 입력은 총 28줄로 각 제출자(학생)의 출석번호 n(1 ≤ n ≤ 30)가 한 줄에 하나씩 주어진다. 출석번호에 중복은 없다.
+ * @output 출력은 2줄이다. 1번째 줄엔 제출하지 않은 학생의 출석번호 중 가장 작은 것을 출력하고, 2번째 줄에선 그 다음 출석번호를 출력한다.
+ * @studyStartDate 2025-07-19
+ * @studyEndDate 2025-07-19
+ */
+public class Problem5597 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int[] arr = new int[30];
+		int temp;
+		
+		for(int i = 0; i < 28; i++) {
+			temp = scan.nextInt();
+			arr[temp - 1] = 1;
+		}
+		
+		for(int i = 0; i < 30; i++) {
+			if (arr[i] == 0) {
+				System.out.println(i + 1);
+			}
+		}
+	}
+}
