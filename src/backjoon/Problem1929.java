c5acd6f7340472bcda5bcb94cefa862abc2738a7
harin1124 <lolrin1124@gmail.com>
2025-08-04 23:33:53 +0900

    1929번 문제 <소수 구하기> 재풀이 완료

--- src/backjoon/Problem1929.java
+++ src/backjoon/Problem1929.java
@@ -12,50 +12,39 @@ import java.util.StringTokenizer;
  * @input 첫째 줄에 자연수 M과 N이 빈 칸을 사이에 두고 주어진다.<br>
            (1 ≤ M ≤ N ≤ 1,000,000) M이상 N이하의 소수가 하나 이상 있는 입력만 주어진다.
  * @output 한 줄에 하나씩, 증가하는 순서대로 소수를 출력한다.
- * @studyStartDate 2022-10-30
- * @studyEndDate 2022-11-02
+ * @studyStartDate 2025-08-04
+ * @studyEndDate 2025-08-04
  */
 public class Problem1929 {
-	public static void main(String[] args)throws Exception{
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer numbers = new StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int a = Integer.parseInt(numbers.nextToken());
-		int b = Integer.parseInt(numbers.nextToken());
-		int[] list = new int[b+1];
-		int temp = 0;
-		int j = 0;
-
-		// 초기화
-		for(temp=0; temp<=b; temp++){
-			list[temp] = temp;
-		}
-		list[1] = 0;
-
-		// 값 하나씩 살펴보기
-		for(temp=2; temp<=b; temp++){
-			// 0보다 큰 수만 반복문 조회한다.
-			// 0이면, 이전 값(temp)으로 반복문 돌 때 temp의 배수여서 지워진 것 => 다시 확인할 필요 없다.
-			if(list[temp] > 0){
-				// temp=2일 경우, 2부터 4, 6, 8로 늘어나기 위해 j+temp로 값을 증가시킨다.
-				for(j=temp; j<=b; j=j+temp){
-					// j와 temp가 같지않다면, 즉 temp=2일때 2가 아닌 4, 6, 8을 0으로 만들기 위함이다.
-					if(j != temp){
-						list[j] = 0;
-					}
+		int m = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		
+		boolean[] isNotPrime = new boolean[n + 1];
+		isNotPrime[0] = true;
+		isNotPrime[1] = true;
+		
+		for (int i = 2; i * i <= n; i++) {
+			if(!isNotPrime[i]) {
+				for(int j = i * i; j <= n; j += i) {
+					isNotPrime[j] = true;
 				}
 			}
 		}
-
-		// 최종 배열 확인 및 0이 아닌 수는 소수이므로 넣기
-		for(temp=a; temp<=b; temp++){
-			if(list[temp] > 0){
-				bw.write(list[temp]+"\n");
+		
+		for(int i = m; i <= n; i++) {
+			if(!isNotPrime[i]) {
+				bw.write(String.valueOf(i));
+				bw.write('\n');
 			}
 		}
-
+		
 		br.close();
+		bw.flush();
 		bw.close();
 	}
 }
